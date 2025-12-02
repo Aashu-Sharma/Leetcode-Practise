84fa2a5ec02f1ce2f15da2f6094412d343f43643
Aashu-Sharma <sujalsharma7804@gmail.com>
2025-12-02 17:38:19 +0530

    had solved earlier somewhere else, just revised it.

---
+++ Contains_Duplicate.js
@@ -0,0 +1,14 @@
+let nums = [1,2,3,4];
+
+const containsDuplicates = (nums) => {
+    let set = new Set();
+    for(let i = 0; i < nums.length; i++){
+        if(set.has(nums[i])){
+            return true;
+        }
+        set.add(nums[i]);
+    }
+    return false;
+};
+
+console.log(containsDuplicates(nums));
