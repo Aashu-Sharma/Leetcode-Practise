dd798f071440bc6ff9f9dd4b2bb15b15a825b2f8
Aashu-Sharma <sujalsharma7804@gmail.com>
2026-01-26 07:32:30 +0530

    optimized the frequency counter solutions a bit.

--- JS_Solutions/Hashing.js
+++ JS_Solutions/Hashing.js
@@ -2,48 +2,48 @@ const lst = [1, 3, 1, 2, 2, 3, 4, 13];
 
 const input_nums = [1, 2, 3, 10, 12, 13];
 
-const find_count_of_elements = (n, list) => {
-    let new_list = new Array(14).fill(0);
-    for (const num of list) {
-        new_list[num] += 1;
-    }
-    console.log(n, new_list[n]);
+let new_list = new Array(14).fill(0);
+for (const num of lst) {
+  new_list[num] += 1;
+}
+
+const find_count_of_elements = (n) => {
+  console.log(n, new_list[n]);
 };
 
 const provide_nums = (input, list) => {
   for (const num of input) {
     if (list.includes(num)) {
-      find_count_of_elements(num, list);
-    }else{
-        console.log(num, 0);
+      find_count_of_elements(num);
+    } else {
+      console.log(num, 0);
     }
   }
 };
 
-// provide_nums(input_nums, lst);
+provide_nums(input_nums, lst);
 
-const my_str = "aashutosh"
-const characters_to_count = ["a", "s", "d", "u", "h"]
+const my_str = "aashutosh";
+const characters_to_count = ["a", "s", "d", "u", "h"];
+let new_list_str = new Array(26).fill(0);
 
-
-const find_character_count = (char, str) => {
-  let new_list = new Array(26).fill(0);
-  for (const c of str) {
-    let index = c.charCodeAt(0) - 'a'.charCodeAt(0);
-    new_list[index] += 1;
-  }
-  console.log(char, new_list[char.charCodeAt(0) - 'a'.charCodeAt(0)]);
+for (const c of my_str) {
+  let index = c.charCodeAt(0) - "a".charCodeAt(0);
+  new_list_str[index] += 1;
 }
 
+const find_character_count = (char) => {
+  console.log(char, new_list_str[char.charCodeAt(0) - "a".charCodeAt(0)]);
+};
 
 const provide_character_to_count = (char_list, str) => {
   for (const char of char_list) {
-    if (str.includes(char)){
-      find_character_count(char, str);
-    }else{
+    if (str.includes(char)) {
+      find_character_count(char);
+    } else {
       console.log(char, 0);
     }
   }
-}
+};
 
-provide_character_to_count(characters_to_count, my_str)
+provide_character_to_count(characters_to_count, my_str);
--- Python_Solutions/Hashing.py
+++ Python_Solutions/Hashing.py
@@ -2,40 +2,41 @@ lst = [1, 3, 1, 2, 2, 3, 4, 13]
 
 input_nums = [1, 2, 3, 10, 12, 13]
 
-def find_count_of_elements(n, lst):
-    new_lst = [0] * 14
-    for i in lst:
-        new_lst[i] += 1
+new_lst = [0] * 14
+for i in lst:
+    new_lst[i] += 1
+
+def find_count_of_elements(n):
     print(n, new_lst[n])
 
 
 def provide_nums(nums, lst):
     for num in nums:
         if num in lst:
-            find_count_of_elements(num, lst) 
+            find_count_of_elements(num) 
         else:
             print(num, 0)
             
 
-# provide_nums(input_nums, lst)
+provide_nums(input_nums, lst)
 
 name = "aashutosh"
 characters_to_count = ["a", "s", "d", "u", "h"]
 
-def find_character_count(char, string):
-    new_lst = [0] * 26
+new_lst_str = [0] * 26
     
-    for i in string:
-        index = ord(i) - ord('a')
-        new_lst[index] += 1
-        
-    print(char, new_lst[ord(char) - ord('a')])
+for i in name:
+    index = ord(i) - ord('a')
+    new_lst_str[index] += 1
+
+def find_character_count(char):    
+    print(char, new_lst_str[ord(char) - ord('a')])
 
 
 def provide_character_to_count(string, char_list):
     for char in char_list:
         if char in string:
-            find_character_count(char, string)
+            find_character_count(char)
         else:
             print(char, 0)
 
