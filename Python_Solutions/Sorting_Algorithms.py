ffe9cca4ea8b8f0f22e431fb2a849c1105b9f197
Aashu-Sharma <sujalsharma7804@gmail.com>
2026-02-02 07:43:29 +0530

    practised seleciton sort, bubble sort, and insertion sort in python

---
+++ Python_Solutions/Sorting_Algorithms.py
@@ -0,0 +1,49 @@
+nums = [65, 46, 24, 52, 20, 9]
+
+# Selection Sort
+
+def selection_sort(lst):
+    for i in range(len(lst)):
+        min = i
+        for j in range(i, len(lst)):
+            if lst[j] < lst[min]:
+                min = j
+                temp = lst[j]
+                lst[j] = lst[i]
+                lst[i] = temp
+    return lst
+
+
+# print(selection_sort(nums))
+
+# bubble sort
+
+
+def bubble_sort(lst):
+    start = len(lst) - 1
+    # reverse travering a list is done this way in python
+    for i in range(start, -1, -1):
+        for j in range(i):
+            if lst[j] > lst[j + 1]:
+                temp = lst[j + 1]
+                lst[j + 1] = lst[j]
+                lst[j] = temp
+    return lst
+
+
+# print(bubble_sort(nums))
+
+# Insertion Sort
+
+def insertion_sort(lst):
+    for i in range(len(lst)):
+        for j in range(i, 0, -1):
+            if lst[j - 1] > lst[j]:
+                temp = lst[j - 1]
+                lst[j - 1] = lst[j]
+                lst[j] = temp
+
+    return lst
+
+
+print(insertion_sort(nums))
