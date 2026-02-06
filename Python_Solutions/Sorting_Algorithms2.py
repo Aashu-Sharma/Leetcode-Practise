8a52ec7964beaeb984f7649eeb78aa1aecbd2cca
Aashu-Sharma <sujalsharma7804@gmail.com>
2026-02-06 17:01:30 +0530

    practiced recursive insertion and bubble sort

--- Python_Solutions/Sorting_Algorithms2.py
+++ Python_Solutions/Sorting_Algorithms2.py
@@ -1,7 +1,7 @@
 # Merge Sort
 # The main idea is to divide the array into halves until we reach an array of size 1.
 
-lst = [7, 4, 1, 5, 3]
+lst = [10, 7, 3, 2, 5]
 
 
 def mergeList(lst, low, mid, high):
@@ -21,7 +21,7 @@ def mergeList(lst, low, mid, high):
     while left <= mid:
         temp.append(lst[left])
         left += 1
-        
+
     while right <= high:
         temp.append(lst[right])
         right += 1
@@ -42,4 +42,45 @@ def merge_sort(lst, low, high):
     return lst
 
 
-print(merge_sort(lst, 0, len(lst) - 1))
+# print(merge_sort(lst, 0, len(lst) - 1))
+
+
+def Recursive_Bubble_Sort(arr, n):
+
+    # base case
+    if n == 1:
+        return
+
+    swapped = 0
+    for i in range(n - 1):
+        if arr[i] > arr[i + 1]:
+            temp = arr[i]
+            arr[i] = arr[i + 1]
+            arr[i + 1] = temp
+            swapped = 1
+
+    if swapped == 0:
+        return arr;
+    
+    Recursive_Bubble_Sort(arr, n - 1)
+    return arr;
+
+
+# print(Recursive_Bubble_Sort(lst, len(lst)))
+
+def Recursive_Insertion_Sort(arr, n):
+    if n == len(arr):
+        return
+    
+    for i in range(n, 0, -1):
+        print(i)
+        if arr[i] < arr[i-1]:
+            temp = arr[i]
+            arr[i] = arr[i-1]
+            arr[i-1] = temp
+
+    Recursive_Insertion_Sort(arr, n+1)
+    return arr;
+    
+
+print(Recursive_Insertion_Sort(lst, 0))
