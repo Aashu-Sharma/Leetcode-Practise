7e0c1b6d3c6e47ddcb43d8a866c2b00b0e0ea877
Aashu-Sharma <sujalsharma7804@gmail.com>
2026-03-19 19:04:36 +0530

    practiced finding majority element using Moore's Voting Algorithm

--- Python_Solutions/Find_Majority_Elem.py
+++ Python_Solutions/Find_Majority_Elem.py
@@ -1,11 +1,31 @@
-nums = [2,2,1,1,1,2,2]
+nums = [2, 2, 1, 1, 1, 2, 2]
 
 def find_majority_elem(nums):
     # better approach using hashmaps. 
-    freq_map = {}
+    # freq_map = {}
+    # for num in nums:
+    #     freq_map[num] = freq_map.get(num, 0) + 1
+    #     if freq_map[num] >= len(nums)/2:
+    #         return num
+
+    #optimal approach. 
+    # Moore's Voting Algorithm
+    elem = nums[0]
+    count = 0
+
     for num in nums:
-        freq_map[num] = freq_map.get(num, 0) + 1
-        if freq_map[num] >= len(nums)/2:
-            return num
+        if num == elem:
+            count+=1
+        else:
+            count-=1
+        
+        if count == 0:
+            elem = num
+            count+=1
+
+    return elem
+
 
 print(find_majority_elem(nums))
+
+# Moore's Voting Algorithm 
