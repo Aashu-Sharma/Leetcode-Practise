0ac347cde7802ecf693fec9833028b3bfb67d0bb
Aashu-Sharma <sujalsharma7804@gmail.com>
2026-03-20 22:07:27 +0530

    revised MergeSort Algorithm in Javascript

--- JS_Solutions/Sorting_Algorithms2.js
+++ JS_Solutions/Sorting_Algorithms2.js
@@ -1,45 +1,48 @@
-let nums = [7, 4, 1, 15, 2, 5, 3, 9];
+let nums = [2, 1, 3, 2, 4];
 
 function mergeSort(arr, low, high) {
-  if (low >= high) return;
+   if(low >= high) return;
 
-  let mid = Math.floor((low + high) / 2);
+   let mid = Math.floor((low + high)/2);
+   mergeSort(arr, low, mid);
+   mergeSort(arr, mid+1, high);
 
-  mergeSort(arr, low, mid);
-  mergeSort(arr, mid + 1, high);
+   mergeList(arr, low, mid, high);
+   return arr;
+}
 
-  let temp = [];
+function mergeList(arr, low, mid, high){
   let left = low;
-  let right = mid + 1;
+  let right = mid+1;
+  let temp = [];
 
-  while (left <= mid && right <= high) {
-    if (arr[left] < arr[right]) {
+  while((left <= mid) && (right <= high)){
+    if(arr[left] <= arr[right]){
       temp.push(arr[left]);
       left++;
-    } else {
+    }else{
       temp.push(arr[right]);
       right++;
     }
   }
 
-  while (left <= mid) {
+  while(left <= mid){
     temp.push(arr[left]);
     left++;
   }
-
-  while (right <= high) {
-    temp.push(arr[right]);
+  while(right <= high){
+    temp.push(arr[right])
     right++;
   }
 
-  for (let i = low; i <= high; i++) {
+  for(let i = low; i <= high; i++){
     arr[i] = temp[i - low];
   }
 
   return arr;
 }
 
-// console.log(mergeSort(nums, 0, nums.length - 1));
+console.log(mergeSort(nums, 0, nums.length - 1));
 
 nums = [4, 5, 1, 2, 7];
 function place_at_correct_position(arr, low, high) {
@@ -76,4 +79,4 @@ function quickSort(arr, low, high) {
   return arr;
 }
 
-console.log(quickSort(nums, 0, nums.length - 1));
+// console.log(quickSort(nums, 0, nums.length - 1));
