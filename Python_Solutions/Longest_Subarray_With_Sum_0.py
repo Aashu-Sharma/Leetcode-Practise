e9ba2df1d06a7a641b7d7c380f3a003e9dd59384
Aashu-Sharma <sujalsharma7804@gmail.com>
2026-05-02 14:38:14 +0530

    revised prefix_sum + hashmap approach and sliding window approaches

--- Python_Solutions/Longest_Subarray_With_Sum_0.py
+++ Python_Solutions/Longest_Subarray_With_Sum_0.py
@@ -1,4 +1,4 @@
-nums, k = [2, -1, 4, 0, 3], 3
+nums, k = [2, 1, 3, 1, 0, 0], 4
 
 def longest_subarray_with_sum_k(nums, k):
     max_length = 0
@@ -12,17 +12,23 @@ def longest_subarray_with_sum_k(nums, k):
     #             max_length = max(max_length, i - a + 1)
     #         print(current_sum, max_length)
 
-    sum_map = {}
-    current_sum = 0
+    pre_sum_map = {}
+    curr_sum = 0
+
     for i in range(len(nums)):
-        current_sum += nums[i]
-        if (current_sum == k):
-            max_length = max(max_length, i + 1)
-        diff = current_sum - k
-        if diff in sum_map:
-            max_length = max(max_length, i - sum_map[diff])
-        if not current_sum in sum_map:
-            sum_map[current_sum] = i
+        curr_sum += nums[i]
+
+        if curr_sum == k:
+            max_length = max(max_length, i+1)
+        
+        diff = curr_sum - k
+
+        if diff in pre_sum_map:
+            max_length = max(max_length, i - pre_sum_map[diff])
+        
+        if not curr_sum in pre_sum_map:
+            pre_sum_map[curr_sum] = i
+    
     return max_length
         
 
--- Python_Solutions/Longest_Subarray_With_Sum_K.py
+++ Python_Solutions/Longest_Subarray_With_Sum_K.py
@@ -1,21 +1,37 @@
-nums, k = [2, 1, 3, 1, 1, 1], 4 
+nums, k = [2, 1, 3, 1, 0, 0], 4 
 
 def find_longest_subarray_with_sum_k(nums, k):
-    i = 0
-    j = 0
-    max_len = 0
-    sum_of_elem = nums[0]
+    # i = 0
+    # j = 0
+    # max_len = 0
+    # sum_of_elem = nums[0]
 
-    while j < len(nums):
-        while sum_of_elem > k:
-            sum_of_elem -= nums[i]
+    # while j < len(nums):
+    #     while sum_of_elem > k:
+    #         sum_of_elem -= nums[i]
+    #         i+=1
+        
+    #     if sum_of_elem == k: 
+    #         max_len = max(max_len, (j - i) + 1)
+    #     j+=1
+    #     if j < len(nums):
+    #         sum_of_elem += nums[j]
+
+    # return max_len
+
+    i = 0
+    curr_sum = 0
+    max_len = 0
+
+    for j in range(len(nums)):
+        curr_sum += nums[j]
+
+        while curr_sum > k:
+            curr_sum -= nums[i]
             i+=1
         
-        if sum_of_elem == k: 
+        if curr_sum == k:
             max_len = max(max_len, (j - i) + 1)
-        j+=1
-        if j < len(nums):
-            sum_of_elem += nums[j]
 
     return max_len
 
